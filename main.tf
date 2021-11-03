bcfa582c8b5d52708aefd4faea9dff321f3b96bc
daku10 <daku.two@gmail.com>
2021-11-03 20:48:48 +0900

    change name

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ locals {
 resource "aws_iam_openid_connect_provider" "github_oidc" {
   url             = "https://token.actions.githubusercontent.com"
   thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
-  client_id_list  = ["https://github.com/${local.github.org}"]
+  client_id_list  = ["https://github.com/${local.github.org}", "sts.amazonaws.com"]
 }
 
 data "aws_iam_policy_document" "github_actions_policy_document" {
