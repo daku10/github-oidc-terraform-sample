20d19a8a560172ab42f931d3ae18ea916ad6622e
daku10 <daku.two@gmail.com>
2022-02-15 00:28:04 +0900

    update thumbprint

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ locals {
 
 resource "aws_iam_openid_connect_provider" "github_oidc" {
   url             = "https://token.actions.githubusercontent.com"
-  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
   client_id_list  = ["https://github.com/${local.github.org}", "sts.amazonaws.com"]
 }
 
